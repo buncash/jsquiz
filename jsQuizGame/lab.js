deb45acb27a563ac019f790c50c6aa106274a3e0
buncash <61774144+buncash@users.noreply.github.com>
2020-04-10 14:58:51 +0530

    Add files via upload

---
+++ jsQuizGame/lab.js
@@ -0,0 +1,15 @@
+
+questions = new Array();
+questions.push("Which among this is a fruit?");
+questions.push("Which among this is a Country?");
+questions.push("Which among this is a vegatable?");
+questions.push("In which year did India get Independence?");
+questions.push("which among is is an Indian city?");
+
+questionOneAnswers = new Array("Apple", "Brinjal", "Potato", "Pumpkin");
+questionTwoAnswers = new Array("India", "Delhi", "Mumbai", "Bangalore");
+questionThreeAnswers = new Array("Banana", "Apple", "Mango", "Mushroom");
+questionFourAnswer = new Array("1947", "1990", "1989", "1999");
+quetionFiveAnswer = new Array("Tokya", "Beijing","Bangalore","Sydney");
+
+correctAnswerIndexes = new Array(0,0,3,0,2);
